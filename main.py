59aeb057344bd176cd21969f08a4441206fdeef6
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2023-11-30 10:19:40 +0600

    'filter_xpath'

--- main.py
+++ main.py
@@ -46,22 +46,20 @@ driver.get(loginURL)
 
 # Load old session into the browser
 loadCookies()
+print("driver.current_url: ", driver.current_url)
 
-if 'my-edit?enter_from=login' not in driver.current_url:
+def login(driver):
     print('Please Login the the website')
-    # print('Press Enter after successful login ...')
-    # input('>: ')
-    try:
-    	WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "signUsername"))).send_keys(email)
-    except Exception as e:
-    	print(e)
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "signUsername"))).send_keys(email)
 
     wait = WebDriverWait(driver, 5)
     continue_button = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "lv-btn-primary")))
     continue_button.click()
     time.sleep(5)
     #-------------
-    driver.find_element(By.CSS_SELECTOR, 'input[type="password"]').send_keys(password)
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[type="password"]'))).send_keys(password)
+    # driver.find_element(By.CSS_SELECTOR, 'input[type="password"]').send_keys(password)
     time.sleep(3)
     #-------------
     sign_in_button = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "lv_sign_in_panel_wide-primary-button")))
@@ -71,34 +69,46 @@ if 'my-edit?enter_from=login' not in driver.current_url:
     # After successful login save new session cookies ot json file
     saveCookies(driver)
 
-else:
+if 'https://www.capcut.com/my-edit?enter_from=login' in driver.current_url:
     print('Previous session loaded')
+else:
+    login(driver)
     
 
 driver.get("https://www.capcut.com/editor?enter_from=create_new&from_page=work_space&__action_from=my_draft&position=my_draft&scenario=tiktok_ads&scale=9%3A16")
 
-wait = WebDriverWait(driver, 3)
-skip_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.guide-modal-footer-btn.guide-modal-footer-skip-btn')))
+skip_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.guide-modal-footer-btn.guide-modal-footer-skip-btn')))
 skip_button.click()
 time.sleep(2)
 
-wait = WebDriverWait(driver, 2)
-upload_button = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "icon-upload-wrap")))
+upload_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "cover-placeholder")))
 upload_button.click()
-time.sleep(10)
+
 #-------------
 try:
-	wait = WebDriverWait(driver, 5)
-	ok_button = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[10]/div[4]/div/button")))
+	ok_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[10]/div[4]/div/button")))
 	ok_button.click()
 	time.sleep(5)
 	print("ok button")
 except Exception as e:
 	print(e)
+#lv-tabs-down-icon
+#scroll down for filter
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "lv-tabs-down-icon"))).click()
+print("scroll working.........")
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "side-tab-filter"))).click()
+print("filter clicked.")
 
+# items_to_click = ["Badbunny", "Tuileries", "Sardinia"]
 
+xpath_selector = f"//div[@class='card-item-label' and text()='{'Sardinia'}']"
+x = f'//*[@id="lv-tabs-4-panel-0"]/div/div/div[1]/div/div/div[2]/div/div[3]/div[1]/div/div[1]/div[1]/div'
+
+WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, x))).click()
+
+print("select filter")
+time.sleep(190)
 # close the browser
 driver.quit()
-
 
 print('Finished ...')
