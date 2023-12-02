b074e8b8b1aa9f5de4c2e7a11bfdd278c4a4a279
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2023-12-02 14:07:49 +0600

    'color_adjustment'

--- constant.py
+++ constant.py
@@ -3,10 +3,30 @@ LOGIN_EMAIL = "alim.abdul.5915@gmail.com"
 LOGIN_PASSWORD = "adminmilon"
 DASHBOARD_URL = "https://www.capcut.com/editor?enter_from=create_new&from_page=work_space&__action_from=my_draft&position=my_draft&scenario=tiktok_ads&scale=9%3A16"
 
+FILTER_NAME = 'Badbunny'  # ["Urbanoid", "Badbunny", "Tuileries", "Sardinia"]
+FOR_1200_WIDTH_VIDEO_BAR=275
+
 POPUP_XPATH = "/html/body/div[10]/div[4]/div/button"
 BASIC_INTENSITY_XPATH = '//*[@id="attribute-panel-wrapper"]/div/div/div[3]/div/div/div/div[2]/div[1]/div/div/div/div[2]/div/span/span/input'
+BASIC_COLOR_ADJUSTMENT_XPATH = '//*[@id="attribute-panel-wrapper"]/div/div/div[3]/div/div/div/div[3]'
+BASIC_COLOR_ADJUSTMENT_COLOR_SATURATION_XPATH='//*[@id="lv-tabs-5-panel-0"]/div/div/div[3]/div[2]/div[2]/div/div[1]/div/div/div/div[2]/div/span/span/input'
+BASIC_COLOR_ADJUSTMENT_COLOR_TEMPERATURE_XPATH='//*[@id="lv-tabs-5-panel-0"]/div/div/div[3]/div[4]/div[2]/div/div[1]/div/div/div/div[2]/div/span/span/input'
+BASIC_COLOR_ADJUSTMENT_LIGHTNESS_BRIGHTNESS_XPATH='//*[@id="lv-tabs-5-panel-0"]/div/div/div[5]/div[2]/div[2]/div/div[1]/div/div/div/div[2]/div/span/span/input'
+BASIC_COLOR_ADJUSTMENT_LIGHTNESS_CONTRAST_XPATH='//*[@id="lv-tabs-5-panel-0"]/div/div/div[5]/div[4]/div[2]/div/div[1]/div/div/div/div[2]/div/span/span/input'
+BASIC_COLOR_ADJUSTMENT_LIGHTNESS_SHINE_XPATH='//*[@id="lv-tabs-5-panel-0"]/div/div/div[5]/div[6]/div[2]/div/div[1]/div/div/div/div[2]/div/span/span/input'
+BASIC_COLOR_ADJUSTMENT_LIGHTNESS_HIGHLIGHT_XPATH='//*[@id="lv-tabs-5-panel-0"]/div/div/div[5]/div[8]/div[2]/div/div[1]/div/div/div/div[2]/div/span/span/input'
+BASIC_COLOR_ADJUSTMENT_LIGHTNESS_SHADOW_XPATH='//*[@id="lv-tabs-5-panel-0"]/div/div/div[5]/div[10]/div[2]/div/div[1]/div/div/div/div[2]/div/span/span/input'
 
-FILTER_NAME = 'Badbunny'  # ["Urbanoid", "Badbunny", "Tuileries", "Sardinia"]
-HORIZONTAL_BAR_WIDTH = 43
-BASIC_INTENSITY_VALUE = 77
-for_1200_width_video_bar = 275
+BASIC_BLEND_OPACITY_XPATH='//*[@id="attribute-panel-wrapper"]/div/div/div[3]/div/div/div/div[5]/div[3]/div[2]/div/div[1]/div/div/div/div[2]/div/span/span/input'
+BASIC_TRANSFORM_SCALE_XPATH='//*[@id="attribute-panel-wrapper"]/div/div/div[3]/div/div/div/div[7]/div[2]/div[2]/div/div[1]/div/div/div/div/div[2]/div/span/span/input'
+BASIC_TRANSFORM_POSITION_X_XPATH='//*[@id="attribute-panel-wrapper"]/div/div/div[3]/div/div/div/div[7]/div[3]/div[2]/div/div[1]/div/div[1]/div/span/span/input'
+BASIC_TRANSFORM_POSITION_Y_XPATH='//*[@id="attribute-panel-wrapper"]/div/div/div[3]/div/div/div/div[7]/div[3]/div[2]/div/div[1]/div/div[2]/div/span/span/input'
+BASIC_TRANSFORM_ROTATE_XPATH='//*[@id="attribute-panel-wrapper"]/div/div/div[3]/div/div/div/div[7]/div[4]/div[2]/div/div[1]/div/div[1]/div/span/span/input'
+BASIC_EFFECT_SHARPNESS_XPATH='//*[@id="lv-tabs-10-panel-0"]/div/div/div[7]/div[2]/div[2]/div/div[1]/div/div/div/div[2]/div/span/span/input'
+BASIC_EFFECT_VIGNETTE_XPATH='//*[@id="lv-tabs-5-panel-0"]/div/div/div[7]/div[4]/div[2]/div/div[1]/div/div/div/div[2]/div/span/span/input'
+BASIC_EFFECT_FADE_XPATH='//*[@id="lv-tabs-5-panel-0"]/div/div/div[7]/div[6]/div[2]/div/div[1]/div/div/div/div[2]/div/span/span/input'
+BASIC_EFFECT_GRAIN_XPATH='//*[@id="lv-tabs-5-panel-0"]/div/div/div[7]/div[8]/div[2]/div/div[1]/div/div/div/div[2]/div/span/span/input'
+
+SMART_TOOLS_XPATH='/html/body/div[8]/div/button[3]'
+SMART_TOOLS_RETOUCH_XPATH='//*[@id="attribute-panel-wrapper"]/div/div/div[3]/div/div/div/div[2]'
+SMART_TOOLS_RETOUCH_BODY_XPATH='//*[@id="attribute-panel-wrapper"]/div/div/div[2]/div/div/div/div/div[1]/div/div/div/div[4]'
